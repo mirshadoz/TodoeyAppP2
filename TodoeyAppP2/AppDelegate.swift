14cf4aaa3decc220b4496b9dd6ba07a4f305a812
Mirshad Ozuturk <limitlessmozrrr@gmail.com>
2018-08-20 19:32:23 +0600

    Setup Realm test Data classs

--- TodoeyAppP2/AppDelegate.swift
+++ TodoeyAppP2/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "Mirshad"
+        data.age = 29
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error creating realm object")
+        }
+        
+        
         return true
     }
 
---
+++ TodoeyAppP2/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  TodoeyAppP2
+//
+//  Created by Mirshad Ozuturk on 8/20/18.
+//  Copyright © 2018 Mirshad Ozuturk. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+    
+}
